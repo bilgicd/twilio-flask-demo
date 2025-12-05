741eb38c181c60226d0bb6a5dfa78556b313b0bf
bilgicd <bilgicd58@gmail.com>
2025-12-05 19:59:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -205,3 +205,13 @@ def confirm_order():
         resp.say("Order cancelled. Thank you for calling Baguette de Moet Andover.")
 
     return str(resp)
+
+
+
+# -------------------------------------------------
+# Run server
+# -------------------------------------------------
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    print(f"DEBUG Flask starting on port {port}")
+    app.run(host="0.0.0.0", port=port)
