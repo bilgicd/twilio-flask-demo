34ccc7163f59c6daa04013ccdaf40e90e8b3aa70
bilgicd <bilgicd58@gmail.com>
2025-12-05 20:17:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -214,9 +214,10 @@ def confirm_order():
     return str(resp)
 
 
-
-# -------------------------------------------------
-# Run server
-# -------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False, use_reloader=False)
+
+    
+
+
